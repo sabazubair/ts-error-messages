24be3d9a53ae59c370002c6f9eb885556f25c91b
Saba <46948491+sabazubair@users.noreply.github.com>
2024-09-10 16:30:41 -0400

    Used optional chaining to handle possibly null type error.

--- src/003-possibly-null-or-undefined.problem.ts
+++ src/003-possibly-null-or-undefined.problem.ts
@@ -2,4 +2,9 @@ const searchParams = new URLSearchParams(window.location.search);
 
 const id = searchParams.get("id");
 
-console.log(id.toUpperCase());
+console.log(id?.toUpperCase());
+
+// optional chaining will convert null to undefined
+// non-null assertion operator is unsafe, i.e. id!.toUpperCase()
+// option #1: use conditional, if (id) { console.log(id.toUpperCase()) }
+// option #2: if(!id){throw new Error("id is required")}

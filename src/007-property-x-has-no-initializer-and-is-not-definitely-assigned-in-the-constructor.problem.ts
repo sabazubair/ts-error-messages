37bd9ab957490f60ae374991a315efcefae54960
Saba <46948491+sabazubair@users.noreply.github.com>
2024-09-12 23:08:08 -0400

    Fixed property x has no initializer error by initializing username in User constructor.

--- src/007-property-x-has-no-initializer-and-is-not-definitely-assigned-in-the-constructor.problem.ts
+++ src/007-property-x-has-no-initializer-and-is-not-definitely-assigned-in-the-constructor.problem.ts
@@ -1,3 +1,17 @@
 class User {
-  private username: string;
+  // private username: string = ''; option 1: initialize username as an empty string
+
+  // private username: string; // option 2: use a constructor to initialize string
+  // constructor() {
+  //   this.username = "";
+  // }
+
+  // private username?: string; // option 3: set username to optional: This is useful when you want username to exist, but you're not sure if it will always have a value.
+
+  // private username!: string; // option 4: use the non-null assertion operator: This tells TypeScript that username will be initialized before it's used. TL;DR: Not initialized right away, but you promise to initialize it before using it.
+
+  private username: string; // recommended option. username is required.
+  constructor(username: string) {
+    this.username = username;
+  }
 }

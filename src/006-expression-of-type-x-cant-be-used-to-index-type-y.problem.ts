bb4485cfa00adb6fb6200ab7d6bca24d7b4388ff
Saba <46948491+sabazubair@users.noreply.github.com>
2024-09-12 22:52:54 -0400

    Resolved indexing error by casting productName as a valid key in productPrices

--- src/006-expression-of-type-x-cant-be-used-to-index-type-y.problem.ts
+++ src/006-expression-of-type-x-cant-be-used-to-index-type-y.problem.ts
@@ -1,3 +1,17 @@
+// Added record utility type. This ensures that any time we add or modify an entry in productPrices, the key must be a string, and the value must be a number.
+// Flexible, safe approach if we were to call getPrice and pass a value that didn't exist like getPrice('Pear').
+
+// const productPrices: Record<string, number> = {
+//   Apple: 1.2,
+//   Banana: 0.5,
+//   Orange: 0.8,
+// };
+
+// const getPrice = (productName: string) => {
+//   return productPrices[productName];
+// };
+
+// Alternatively, if productPrices is a constant object, we can use the following approach:
 const productPrices = {
   Apple: 1.2,
   Banana: 0.5,
@@ -5,5 +19,6 @@ const productPrices = {
 };
 
 const getPrice = (productName: string) => {
-  return productPrices[productName];
+  // cast productName to keyof typeof productPrices to ensure that the key exists in productPrices
+  return productPrices[productName as keyof typeof productPrices];
 };
